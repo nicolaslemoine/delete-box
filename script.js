ad31206a99c037ff6f2ffc24eec1539f9e83e2cf
Nicolas Lemoine <nilemoine@gmail.com>
2017-07-08 17:34:30 +0200

    Improve JS

--- script.js
+++ script.js
@@ -2,30 +2,31 @@
 $(document).ready(function(){
 
 	// Confirm delete
-    $('.delete-action').click(function() {
+    var $deleteAction = $('.delete-action');
+    $deleteAction.click(function() {
         //Get the path of the href
-        var item_link = $(this).attr('href');
+        var itemLink = $deleteAction.attr('href');
 
         //Select the <tr> and get the height
-        var item_tr = $(this).closest('tr');
-        var item_tr_height = item_tr.outerHeight();
-        var item_tr_value = item_tr.html();
+        var itemTr = $deleteAction.closest('tr');
+        var itemTrHeight = itemTr.outerHeight();
+        var itemTrValue = itemTr.html();
 
         //Display the confirm box in the <tr>
-        item_tr.empty();
-        item_tr.append('<td colspan="3" height="'+item_tr_height+'" class="delete-box">\
+        itemTr.empty();
+        itemTr.append('<td colspan="3" height="'+itemTrHeight+'" class="delete-box">\
         	Are you sure you want to delete this entry? <a href="'+item_link+'" class="delete-yes">Yes</a><a class="delete-no">No</a>\
         </td>');
 
         //Proceed with delete
         $('.delete-yes').click(function() {
-        	item_tr.remove();
+        	itemTr.remove();
             return false;
         });
 
         //Do not delete, remove the box
         $('.delete-no').click(function() {
-            item_tr.empty().append(item_tr_value);
+            itemTr.empty().append(itemTrValue);
 
             return false;
         });
