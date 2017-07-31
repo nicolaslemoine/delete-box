0dad027fd0bbc848c259296e5029a71bfb61d8a8
Nicolas Lemoine <nilemoine@gmail.com>
2017-07-31 19:04:16 +0200

    The box adjusts to the number of td of the table

--- script.js
+++ script.js
@@ -6,14 +6,17 @@ $(document).ready(function(){
     var deleteAction = function() {
         //Select the <tr> and get height and value
         var itemTr = $(this).closest('tr');
+        var itemTrTdLength = itemTr.children('td').length;
         var itemTrHeight = itemTr.outerHeight();
         var itemTrValue = itemTr.html();
 
 
         //Display the confirm box in the <tr>
-        itemTr.empty().append('<td colspan="3" height="'+itemTrHeight+'" class="delete-box">\
-            Êtes-vous sûr de vouloir supprimer cet élément ? <a class="delete-yes">Oui</a><a class="delete-no">Non</a>\
-        </td>');
+        itemTr.empty().append(
+            '<td colspan="'+itemTrTdLength+'" height="'+itemTrHeight+'" class="delete-box">'+
+            'Êtes-vous sûr de vouloir supprimer cet élément ? <a class="delete-yes">Oui</a><a class="delete-no">Non</a>'+
+            '</td>'
+        );
 
         //Proceed with delete
         $('.delete-yes').click(function() {
